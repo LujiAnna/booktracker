2e38c6f63b5e0e40bbc8f8e14156743c8d160ca1
Eva Roets <84382812+EvaRoets@users.noreply.github.com>
2021-09-06 13:27:27 +0200

    Add routing to home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,13 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Travel;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function index()
-    {
-        return view('home');
-
-    }
+    //
+}
