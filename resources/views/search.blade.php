0f5f144857ae5ee9af66e729ec4ce4abfcc7c008
Anna Ikoki <ikokianna@gmail.com>
2021-09-06 16:24:49 +0200

    Refactor search page with layout sections

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -1,18 +1,6 @@
+@extends('layout')
 
-        <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
-            @if (Route::has('login'))
-                <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
-                    @auth
-                        <a href="{{ url('/home') }}" class="text-sm text-gray-700 underline">Home</a>
-                    @else
-                        <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Log in</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
+@section('content')
 
         <div class="container">
           <div class="col-md-7 col-lg-8">
@@ -52,4 +40,4 @@
         {{-- (top 4 most recent books) --}}
         <h2>Recent Books:</h2>
         <hr class="my-4"> 
-
+@endsection
