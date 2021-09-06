5ae94ce2494a29466c1cec9c24cfceff37d50dbf
Eva Roets <84382812+EvaRoets@users.noreply.github.com>
2021-09-06 13:28:26 +0200

    Fix home routing

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,5 +6,9 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('home');
+
+    }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,14 +1,2 @@
-<?php
 
-namespace App\Http\Controllers;
 
-use App\Models\Travel;
-use Illuminate\Http\Request;
-
-class HomeController extends Controller
-{
-    public function index()
-    {
-        return view('home');
-
-    }
