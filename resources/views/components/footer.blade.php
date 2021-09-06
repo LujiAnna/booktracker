78613c8ff53395c73e8e9027b2ddf09320789521
Eva Roets <84382812+EvaRoets@users.noreply.github.com>
2021-09-06 13:33:49 +0200

    Add footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,1 +1,6 @@
-
+<footer class="fixed-bottom">
+    <p style="text-align: center"> Copyright &copy;
+        <script>document.write(new Date().getFullYear())</script>
+        Anna Ikoki & Eva Roets
+    </p>
+</footer>
