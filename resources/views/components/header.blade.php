6a06dc4ef964556cdd7872a5addb645f755ced4e
Eva Roets <84382812+EvaRoets@users.noreply.github.com>
2021-09-07 15:21:44 +0200

    Add search bar to home

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,12 +1,12 @@
 <header>
     <nav class="navbar navbar-expand-lg navbar-light bg-light navbar-fixed-top ">
         <div class="container-fluid">
-            <a class="navbar-brand" href="{{ route('home') }}">
-                Home
-            </a>
+{{--            <a class="navbar-brand" style="text-align: center" href="{{ route('') }}">--}}
+{{--                TBD--}}
+{{--            </a>--}}
             <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                 <div class="navbar navbar-light">
-                    <a class="nav-link active" aria-current="page" href="{{ route('welcome') }}">Welcome</a>
+                    <a class="nav-link active" style="text-align: center" aria-current="page" href="{{ route('home') }}">Your book tracker</a>
                 </div>
             </div>
         </div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,10 +2,97 @@
 
 @section('content')
 
-    <div class="container justify-content-center">
+<div class="bg-image h-100"
+     style="background-image: url(https://mdbootstrap.com/img/Photos/new-templates/search-box/img6.jpg); ">
+    <div class="mask d-flex align-items-center h-100" style="background-color: rgba(0,0,0,.43);">
+        <div class="container" style="margin: 100px auto 100px auto">
+{{--            <div class="row">--}}
+                <div class="col-md-10 col-lg-8 col-xl-7 mx-auto">
+                    <div class="card mb-2">
+                        <div class="card-body p-2">
+                            <div class="input-group input-group-lg">
+                                <input type="text" class="form-control form-control-lg rounded"
+                                       placeholder="Search for your favourite book..."
+                                       aria-label="Type Keywords" aria-describedby="basic-addon2"/>
+                                <span class="input-group-text border-0" id="basic-addon2"><i
+                                        class="fas fa-search"></i></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card text-center bg-black">
 
-{{--        TODO Add search field--}}
-    <p>This is the home page</p>
+{{--                                TODO: activate collapse--}}
+
+                        <div class="card-body p-4">
+                            <div class="row">
+                                <button type="button" class="btn btn-warning" style="align-items: center; margin: 20px 0 20px 0"
+                                    {{--                                            data-toggle="collapse"--}}
+                                    {{--                                            data-target="#collapseAdvanced" aria-expanded="false"--}}
+                                    {{--                                            aria-controls="collapseAdvanced"--}}
+                                >
+                                    Advanced search
+                                </button>
+                            </div>
+
+{{--                                    <div class="collapse" id="collapseAdvanced">--}}
+                                <div class="row">
+                                    <div class="col-md-3 mb-4">
+                                        <div class="form-check form-check-inline">
+                                            <input class="form-check-input" type="checkbox" id="inlineCheckbox1"
+                                                   value="option1"
+                                                   checked/>
+                                            <label class="form-check-label text-white font-weight-bold"
+                                                   for="inlineCheckbox1">ALL</label>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-3 mb-4">
+                                        <div class="form-check form-check-inline">
+                                            <input class="form-check-input" type="checkbox" id="inlineCheckbox2"
+                                                   value="option2"/>
+                                            <label class="form-check-label text-white font-weight-bold"
+                                                   for="inlineCheckbox2">TITLE</label>
+                                        </div>
+                                    </div>
+
+                                    <div class="col-md-3 mb-4">
+                                        <div class="form-check form-check-inline">
+                                            <input class="form-check-input" type="checkbox" id="inlineCheckbox2"
+                                                   value="option2"/>
+                                            <label class="form-check-label text-white font-weight-bold"
+                                                   for="inlineCheckbox2">AUTHOR</label>
+                                        </div>
+                                    </div>
+
+                                    <div class="col-md-3 mb-4">
+                                        <div class="form-check form-check-inline">
+                                            <input class="form-check-input" type="checkbox" id="inlineCheckbox2"
+                                                   value="option2"/>
+                                            <label class="form-check-label text-white font-weight-bold"
+                                                   for="inlineCheckbox2">GENRE</label>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="d-flex justify-content-between align-items-center mt-4">
+                                        <p class="text-white mb-0"><span class="text-warning">108 </span>results
+                                        </p>
+                                        <div>
+                                            <button type="button" class="btn btn-link text-white"
+                                                    data-mdb-ripple-color="dark">Reset
+                                            </button>
+                                            <button type="button" class="btn btn-warning">Search</button>
+                                        </div>
+                                    </div>
+                                </div>
+{{--                                    </div>--}}
+                        </div>
+                    </div>
+                </div>
+{{--            </div>--}}
+        </div>
     </div>
+</div>
 
 @endsection
+
+
