069b690195b1ed3008cbd1c773db4d7c19ae4002
Eva Roets <84382812+EvaRoets@users.noreply.github.com>
2021-09-08 12:16:07 +0200

    Link styling correctly

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,6 +6,7 @@
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+    <link rel="stylesheet" href="./app.css">
     <title>Book tracker</title>
     <!-- Styles -->
     <style>
