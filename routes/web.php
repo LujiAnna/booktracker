2d6548ccfaf2af38c748e415931927286126f62f
Eva Roets <84382812+EvaRoets@users.noreply.github.com>
2021-09-09 04:24:21 +0200

    WIP Add routing submit search

--- routes/web.php
+++ routes/web.php
@@ -2,20 +2,21 @@
 
 use App\Http\Controllers\Controller;
 use App\Http\Controllers\HomeController;
-
 use App\Http\Controllers\SearchController;
 use App\Http\Controllers\ExactSearchController;
-
 use Illuminate\Support\Facades\Route;
 
+
 Route::get('/', [Controller::class, 'index'])->name('welcome');
-
 Route::get('home', [HomeController::class, 'index'])->name('home');
-
 Route::get('search', [SearchController::class, 'show'])->name('search');
 
 
 // add slug and name with 2 variables
 // Route::get('search', [SearchController::class, 'submitSearch'])->name('submitSearch');
 
-Route::get('exactsearch', [ExactSearchController::class, 'exactsearch'])->name('exactsearch');
+Route::get('exactsearch', [ExactSearchController::class, 'submitSearch'])->name('exactsearch');
+
+//Route::get('exactsearch', function(){
+//    dd(request('exactsearch'));
+//});
