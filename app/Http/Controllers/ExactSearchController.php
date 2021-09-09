33fce0c815f662f1dfd97b575e22fc04cd406ab3
Eva Roets <84382812+EvaRoets@users.noreply.github.com>
2021-09-09 04:23:18 +0200

    WIP Add submit search

--- app/Http/Controllers/ExactSearchController.php
+++ app/Http/Controllers/ExactSearchController.php
@@ -3,13 +3,34 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class ExactSearchController extends Controller
 {
-    public function index()
+    public function show()
     {
         return view('exactsearch');
     }
 
 
+    //TODO verify code here
+    public function submitSearch(Request $request)
+    {
+        $input = request('exactsearch');
+        if ($input) {
+            $client = new Client();
+            $googleBooks = Http::get('https://www.googleapis.com/books/v1/volumes?q=' . $input . '&key=' . config('services.google.key'));
+
+            $res = $client->request('GET', "https://www.googleapis.com/books/v1/volumes?q=" . $input . "&key=" . config('services.google.key'));
+
+            if ($res->getStatusCode() == 200) {
+                $res->getBody();
+                dd(json_decode($res->getBody()->getContents()));
+            }
+
+            return view('exactsearch')->with('name', $googleBooks);
+        } else {
+            return view('exactsearch');
+        }
+    }
 }
