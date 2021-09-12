cfd746432db750a6a2f0a16a8ba73e9660cb1326
Eva Roets <84382812+EvaRoets@users.noreply.github.com>
2021-09-12 14:29:54 +0200

    Refactor/delete exact search

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,6 @@
 use App\Http\Controllers\Controller;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\SearchController;
-use App\Http\Controllers\ExactSearchController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
@@ -13,13 +12,3 @@ Route::get('search', [SearchController::class, 'list'])->name('search');
 
 // add slug and name with 2 variables
 Route::get('search', [SearchController::class, 'submitSearch'])->name('submitSearch');
-
-Route::get('exactsearch', [ExactSearchController::class, 'list'])->name('list');
-Route::get('exactsearch', [ExactSearchController::class, 'submitSearch'])->name('exactsearch');
-
-
-
-
-
-
-
